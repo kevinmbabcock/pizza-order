15b1fdf741aa232dfffb3501e875eaefdb4c0667
Kevin Babcock <kevinmbabcock@icloud.com>
2017-11-03 11:21:03 -0700

    take topping input and place into array inside pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,18 @@ $(document).ready(function() {
   $("form#newPizza").submit(function(event) {
     event.preventDefault();
 
-    alert("test");
     var crust = $("#crust").val();
     var sauce = $("#sauce").val();
     var crustFlavor = $("crustFlavor");
 
     var newPizza = new Pizza(crust, sauce, crustFlavor);
 
-    console.log(newPizza.crust);
-    console.log(newPizza.sauce);
-    console.log(newPizza.crustFlavor);
+    //console.log(newPizza.toppings);
+    $(".newTopping").each(function() {
+      var topping = $(".topping").val();
+      console.log(topping);
+      newPizza.toppings.push(topping);
+      console.log(newPizza.toppings);
+    })
   });
 });
